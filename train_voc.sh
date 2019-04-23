4737090c5affed1cc407391ab5b8849ac5916bb8
MenghaoGuo <38318671+MenghaoGuo@users.noreply.github.com>
2019-04-23 22:50:50 +0800

    Update train_voc.sh

--- train_voc.sh
+++ train_voc.sh
@@ -1,3 +1,3 @@
 CUDA_VISIBLE_DEVICES=0,1,2,3 python train_autodeeplab.py --backbone resnet --lr 0.007 
---workers 4 --use-sbd True --epochs 50 --batch-size 16 --gpu-ids 0,1,2,3 --checkname 
+--workers 4 --use-sbd True --epochs 50 --batch-size 2 --gpu-ids 0,1,2,3 --checkname 
 deeplab-resnet --eval-interval 1 --dataset pascal
