9000048abefa7e8869d9a97340cba5c7b2a5c16e
Hank Kung <hank910140@gmail.com>
2019-06-17 05:41:57 +0800

    Fixed the bug in dataloader
    train_loader should return train_loader twice for alpha and beta training during archtecture search

--- dataloaders/__init__.py
+++ dataloaders/__init__.py
@@ -15,7 +15,7 @@ def make_data_loader(args, **kwargs):
         val_loader = DataLoader(val_set, batch_size=args.batch_size, shuffle=False, **kwargs)
         test_loader = None
 
-        return train_loader, val_loader, test_loader, num_class
+        return train_loader, train_loader, val_loader, test_loader, num_class
 
     elif args.dataset == 'cityscapes':
         train_set = cityscapes.CityscapesSegmentation(args, split='train')
@@ -26,7 +26,7 @@ def make_data_loader(args, **kwargs):
         val_loader = DataLoader(val_set, batch_size=args.batch_size, shuffle=False, **kwargs)
         test_loader = DataLoader(test_set, batch_size=args.batch_size, shuffle=False, **kwargs)
 
-        return train_loader, val_loader, test_loader, num_class
+        return train_loader, train_loader, val_loader, test_loader, num_class
 
     elif args.dataset == 'coco':
         train_set = coco.COCOSegmentation(args, split='train')
@@ -35,7 +35,7 @@ def make_data_loader(args, **kwargs):
         train_loader = DataLoader(train_set, batch_size=args.batch_size, shuffle=True, **kwargs)
         val_loader = DataLoader(val_set, batch_size=args.batch_size, shuffle=False, **kwargs)
         test_loader = None
-        return train_loader, val_loader, test_loader, num_class
+        return train_loader, train_loader, val_loader, test_loader, num_class
 
     else:
         raise NotImplementedError
