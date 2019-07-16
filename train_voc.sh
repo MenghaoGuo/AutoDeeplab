017c9dcc35c8ef3f7fd096947e52b087c8e3a6bb
Hank Kung <hank910140@gmail.com>
2019-07-16 16:35:15 +0800

    The script
    replace hyphen with underscore

--- train_voc.sh
+++ train_voc.sh
@@ -1,3 +1,1 @@
-CUDA_VISIBLE_DEVICES=0,1,2,3 python train_autodeeplab.py --backbone resnet --lr 0.007 
---workers 4 --use-sbd True --epochs 50 --batch-size 2 --gpu-ids 0,1,2,3 --checkname 
-deeplab-resnet --eval-interval 1 --dataset pascal
+CUDA_VISIBLE_DEVICES=0,1,2,3 python train_autodeeplab.py --backbone resnet --lr 0.007 --workers 4 --epochs 40 --batch_size 2 --gpu_ids 0,1,2,3 --eval_interval 1 --dataset cityscapes
