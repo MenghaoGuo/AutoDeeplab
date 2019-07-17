0f8fbe6c6942ea36f4606ab5a08031f677447ef2
Randylcy <33777388+Randylcy@users.noreply.github.com>
2019-07-17 16:46:26 +0800

    Update __init__.py

--- dataloaders/__init__.py
+++ dataloaders/__init__.py
@@ -25,9 +25,10 @@ def make_data_loader(args, **kwargs):
         train_loader1 = DataLoader(train_set1, batch_size=args.batch_size, shuffle=True, **kwargs)
         train_loader2 = DataLoader(train_set2, batch_size=args.batch_size, shuffle=True, **kwargs)
         val_loader = DataLoader(val_set, batch_size=args.batch_size, shuffle=False, **kwargs)
-        test_loader = DataLoader(test_set, batch_size=args.batch_size, shuffle=False, **kwargs)
+        #test_loader = DataLoader(test_set, batch_size=args.batch_size, shuffle=False, **kwargs)
 
-        return train_loader1, train_loader2, val_loader, test_loader, num_class
+        #return train_loader1, train_loader2, val_loader, test_loader, num_class
+        return train_loader1, train_loader2, val_loader, num_class
 
     elif args.dataset == 'coco':
         train_set = coco.COCOSegmentation(args, split='train')
