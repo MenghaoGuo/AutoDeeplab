63b425d8bb7874f19e093221f3e312d9b452e73f
root <yuan1998jeff@gmail.com>
2021-10-02 22:04:19 +0000

    cityscape ready

--- mypath.py
+++ mypath.py
@@ -6,7 +6,7 @@ class Path(object):
         elif dataset == 'sbd':
             return '/path/to/datasets/benchmark_RELEASE/'  # folder that contains dataset/.
         elif dataset == 'cityscapes':
-            return '/path/to/datasets/cityscapes/'     # foler that contains leftImg8bit/
+            return '/jiajian-siyuan-volume/cityscapes/'     # foler that contains leftImg8bit/
         elif dataset == 'coco':
             return '/path/to/datasets/coco/'
         else:
