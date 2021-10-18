3959495a04b8ec7d528ad46232a05a1e8530e8c9
Siyuan Zhu <yuan1998jeff@163.com>
2021-10-18 14:21:31 -0700

    8 layers

--- train_voc.sh
+++ train_voc.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=0,1,2,3 python train_autodeeplab.py --backbone resnet --lr 0.007 --workers 4 --epochs 40 --batch_size 1 --gpu_ids 0,1,2,3 --eval_interval 1 --dataset cityscapes --crop_size 256 --num_layers 6
+CUDA_VISIBLE_DEVICES=0,1,2,3 python train_autodeeplab.py --backbone resnet --lr 0.007 --workers 4 --epochs 40 --batch_size 1 --gpu_ids 0,1,2,3 --eval_interval 1 --dataset cityscapes --crop_size 256 --num_layers 8
